278d45785f419c047d550d2e87b98a641d57a6c8
SunYiwen <921191828@qq.com>
2020-02-24 13:59:44 +0800

    update the way to get tags and types

--- front-end/src/components/Passage.vue
+++ front-end/src/components/Passage.vue
@@ -3,12 +3,8 @@
     <h1 class="sun_title">{{ passage.title }}</h1>
     <ul>
       <li><b-icon-calendar></b-icon-calendar>{{ create_time }}</li>
-      <li v-for="type in types" v-bind:key="type._id" ><span v-if="type.type_id===passage.type_id">
-        <b-icon-folder></b-icon-folder>{{ type.type_name }}
-      </span></li>
-      <li v-for="tag in tags" v-bind:key="tag._id"><span v-if="tag.tag_id===passage.tag_id">
-        <b-icon-tag></b-icon-tag>{{ tag.tag_name}}
-      </span></li>
+      <li><b-icon-folder></b-icon-folder>{{ getTypeName }}</li>
+      <li><b-icon-tag></b-icon-tag>{{ getTagName }}</li>
       <li><b-icon-eye></b-icon-eye>12</li>
     </ul>
     <p class="description">{{ passage.description }}...</p>
@@ -25,7 +21,15 @@ export default {
     types: Array,
     tags: Array
   },
+  data () {
+    return {
+      // typeName: '',
+      // tagName: ''
+    }
+  },
   mounted () {
+  },
+  created () {
   },
   methods: {
     showBlog: function () {
@@ -44,6 +48,28 @@ export default {
       time = time.split('-')
       const Time = parseInt(time[0]) + '年' + parseInt(time[1]) + '月' + parseInt(time[2]) + '日'
       return Time
+    },
+    getTypeName: function () {
+      const typeId = this.passage.type_id
+      const type = this.types.find(function (item) {
+        return item.type_id === typeId
+      })
+      if (type === undefined) {
+        return ''
+      } else {
+        return type.type_name
+      }
+    },
+    getTagName: function () {
+      const tagId = this.passage.tag_id
+      const tag = this.tags.find(function (item) {
+        return item.tag_id === tagId
+      })
+      if (tag === undefined) {
+        return ''
+      } else {
+        return tag.tag_name
+      }
     }
   }
 }
@@ -69,14 +95,13 @@ export default {
     margin: 10px;
   }
   ul{
-    float: left;
+    //float: left;
   }
   li{
     list-style-type:none;
     display:inline;
     margin-right:10px;
     font-size: 13px;
-    margin-right: 10px;
     margin-bottom: 20px;
     color: #777676;
   }
